25a7f463bb2bfb3b7bf95d4b4ed0f1253a879d2b
Jeanette Nguyen <jeanettenguyen@jeanettes-air.home>
2018-06-09 08:37:23 -0700

    gen hist

---
+++ HW6_scripts/generate_hist.py
@@ -0,0 +1,48 @@
+import pickle
+import matplotlib.pyplot as plt
+import numpy as np
+
+def main():
+    with open("3a_layer_outputs.pkl", "rb") as f:
+        outputs = pickle.load(f)
+
+    in_bn1 = outputs['layer1'].cpu().data.numpy().reshape(-1, 1)
+    in_bn3 = outputs['layer3'].cpu().data.numpy().reshape(-1, 1)
+    in_bn5 = outputs['layer5'].cpu().data.numpy().reshape(-1, 1)
+    in_bn7 = outputs['layer7'].cpu().data.numpy().reshape(-1, 1)
+    GetHistogram(in_bn1, '1st', '')
+    GetHistogram(in_bn3, '2nd', '')
+    GetHistogram(in_bn5, '3rd', '')
+    GetHistogram(in_bn7, '7th', '')
+    
+    mean = [np.mean(in_bn1), np.mean(in_bn3), np.mean(in_bn5), np.mean(in_bn7)]
+    var = [np.var(in_bn1), np.var(in_bn3), np.var(in_bn5), np.var(in_bn7)]
+    PlotCharacterics('Mean', mean)
+    PlotCharacterics('Variance', var)
+
+    GetHistogram((in_bn1-mean[0])/var[0], '1st', '_norm')
+    GetHistogram((in_bn3-mean[1])/var[1], '2nd', '_norm')
+    GetHistogram((in_bn5-mean[2])/var[2], '3rd', '_norm')
+    GetHistogram((in_bn6-mean[3])/var[3], '7th', '_norm')  
+
+def PlotCharacterics(type, data):
+    fname = '3a'+type+'.png'
+    plt.figure()
+    plt.xlabel("Layer")
+    plt.ylabel(type)
+    plt.title("Running "+type)
+    plt.plot([1,3,5,7], data)
+
+def GetHistogram(output, label, norm):
+
+    fname = '3a_hist_layer'+layer+norm+'.png'
+    plt.figure()
+    plt.xlabel("")
+    plt.ylabel("Count")
+    plt.title("Histogram of " + label + " Layer")
+    plt.hist(output, 50) 
+    plt.savefig(fname)
+
+
+if __name__ == '__main__':
+    main()
